010a508625a673c7f87af15bb61a9773c5bfa070
Raja-fn <businessrexxon@gmail.com>
2026-03-19 04:27:26 +0530

    feat: implement scanner api and integrate bandit and zap scanners

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI, Body
-from backend.llm_engine.test_llm import process_vulnerabilities
+from backend.api.scan import router as scan_router
 
 app = FastAPI()
+
+# include routes
+app.include_router(scan_router)
+
 
 @app.get("/")
 def home():
     return {"message": "ARKA Auditor Backend Running"}
 
+
 @app.post("/fix")
 def fix(vulns: list = Body(...)):
-    return process_vulnerabilities(vulns)
+    return vulns  # temporary (LLM later)
